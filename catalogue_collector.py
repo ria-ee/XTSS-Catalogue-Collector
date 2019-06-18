a0ff390a8cc3a47230bbc8821402d4ca7b5a61e6
Vitali Stupin <vitali.stupin@ria.ee>
2019-06-18 12:33:15 +0300

    Fix some lint problems

--- catalogue_collector.py
+++ catalogue_collector.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""This is a module for collection of X-Road services information."""
+
 import queue
 from threading import Thread, Event, Lock
 import argparse
@@ -60,28 +62,31 @@ DEFAULT_LOGGER = {
 }
 
 # Application will use this logger
-logger = logging.getLogger('catalogue-collector')
+LOGGER = logging.getLogger('catalogue-collector')
 
 
 def load_config(config_file):
+    """Load configuration from JSON file"""
     try:
-        with open(config_file, 'r') as f:
-            return json.load(f)
+        with open(config_file, 'r') as conf:
+            return json.load(conf)
     except IOError as err:
-        logger.error('Cannot load configuration file "%s": %s', config_file, str(err))
+        LOGGER.error('Cannot load configuration file "%s": %s', config_file, str(err))
         return None
     except json.JSONDecodeError as err:
-        logger.error('Invalid JSON configuration file "%s": %s', config_file, str(err))
+        LOGGER.error('Invalid JSON configuration file "%s": %s', config_file, str(err))
         return None
 
 
 def configure_logging(config):
+    """Configure logging based on loaded configuration"""
     if 'logging-config' in config:
         logging.config.dictConfig(config['logging-config'])
-        logger.info('Logger configured')
+        LOGGER.info('Logger configured')
 
 
 def set_params(config):
+    """Configure parameters based on loaded configuration"""
     params = {
         'path': None,
         'url': None,
@@ -100,81 +105,84 @@ def set_params(config):
 
     if 'output_path' in config:
         params['path'] = config['output_path']
-        logger.info('Configuring "path": %s', params['path'])
+        LOGGER.info('Configuring "path": %s', params['path'])
     else:
-        logger.error('Configuration error: Output path is not provided')
+        LOGGER.error('Configuration error: Output path is not provided')
         return None
 
     if 'server_url' in config:
         params['url'] = config['server_url']
-        logger.info('Configuring "url": %s', params['url'])
+        LOGGER.info('Configuring "url": %s', params['url'])
     else:
-        logger.error('Configuration error: Local Security Server URL is not provided')
+        LOGGER.error('Configuration error: Local Security Server URL is not provided')
         return None
 
     if 'client' in config and len(config['client']) in (3, 4):
         params['client'] = config['client']
-        logger.info('Configuring "client": %s', params['client'])
+        LOGGER.info('Configuring "client": %s', params['client'])
     else:
-        logger.error(
+        LOGGER.error(
             'Configuration error: Client identifier is incorrect. Expecting list of identifiers. '
             'Example: ["INST", "CLASS", "MEMBER_CODE", "MEMBER_CLASS"])')
         return None
 
     if 'instance' in config and config['instance']:
         params['instance'] = config['instance']
-        logger.info('Configuring "instance": %s', params['instance'])
+        LOGGER.info('Configuring "instance": %s', params['instance'])
 
     if 'timeout' in config and config['timeout'] > 0.0:
         params['timeout'] = config['timeout']
-        logger.info('Configuring "timeout": %s', params['timeout'])
+        LOGGER.info('Configuring "timeout": %s', params['timeout'])
 
     if 'server_cert' in config and config['server_cert']:
         params['verify'] = config['server_cert']
-        logger.info('Configuring "verify": %s', params['verify'])
+        LOGGER.info('Configuring "verify": %s', params['verify'])
 
     if 'client_cert' in config and 'client_key' in config \
             and config['client_cert'] and config['client_key']:
         params['cert'] = (config['client_cert'], config['client_key'])
-        logger.info('Configuring "cert": %s', params['cert'])
+        LOGGER.info('Configuring "cert": %s', params['cert'])
 
     if 'thread_count' in config and config['thread_count'] > 0:
         params['thread_cnt'] = config['thread_count']
-        logger.info('Configuring "thread_cnt": %s', params['thread_cnt'])
+        LOGGER.info('Configuring "thread_cnt": %s', params['thread_cnt'])
 
     if 'wsdl_replaces' in config:
         params['wsdl_replaces'] = config['wsdl_replaces']
-        logger.info('Configuring "wsdl_replaces": %s', params['wsdl_replaces'])
+        LOGGER.info('Configuring "wsdl_replaces": %s', params['wsdl_replaces'])
 
-    logger.info('Configuration done')
+    LOGGER.info('Configuration done')
 
     return params
 
 
 def make_dirs(path):
+    """Create directories if they do not exist"""
     try:
         os.makedirs(path)
     except OSError:
         pass
     if not os.path.exists(path):
-        logger.error('Cannot create directory "%s"', path)
+        LOGGER.error('Cannot create directory "%s"', path)
         return False
     return True
 
 
 def hash_wsdls(path):
+    """Find hashes of all WSDL's in directory"""
     hashes = {}
     for file_name in os.listdir(path):
-        s = re.search('^(\\d+)\\.wsdl$', file_name)
-        if s:
+        search_res = re.search('^(\\d+)\\.wsdl$', file_name)
+        if search_res:
             # Reading as bytes to avoid line ending conversion
-            with open('{}/{}'.format(path, file_name), 'rb') as fh:
-                wsdl = fh.read()
+            with open('{}/{}'.format(path, file_name), 'rb') as wsdl_file:
+                wsdl = wsdl_file.read()
             hashes[file_name] = hashlib.md5(wsdl).hexdigest()
     return hashes
 
 
 def save_wsdl(path, hashes, wsdl, wsdl_replaces):
+    """Save WSDL if it does not exist yet"""
     # Replacing dynamically generated comments in WSDL to avoid new WSDL
     # creation because of comments.
     for wsdl_replace in wsdl_replaces:
@@ -185,20 +193,21 @@ def save_wsdl(path, hashes, wsdl, wsdl_replaces):
         if wsdl_hash == hashes[file_name]:
             # Matching WSDL found
             return file_name, hashes
-        s = re.search('^(\\d+)\\.wsdl$', file_name)
-        if s:
-            if int(s.group(1)) > max_wsdl:
-                max_wsdl = int(s.group(1))
+        search_res = re.search('^(\\d+)\\.wsdl$', file_name)
+        if search_res:
+            if int(search_res.group(1)) > max_wsdl:
+                max_wsdl = int(search_res.group(1))
     # Creating new file
     new_file = '{}.wsdl'.format(int(max_wsdl) + 1)
     # Writing as bytes to avoid line ending conversion
-    with open('{}/{}'.format(path, new_file), 'wb') as f:
-        f.write(wsdl.encode('utf-8'))
+    with open('{}/{}'.format(path, new_file), 'wb') as wsdl_file:
+        wsdl_file.write(wsdl.encode('utf-8'))
     hashes[new_file] = wsdl_hash
     return new_file, hashes
 
 
 def worker(params):
+    """Main function for worker threads"""
     while True:
         # Checking periodically if it is the time to gracefully shutdown
         # the worker.
@@ -207,8 +216,7 @@ def worker(params):
         except queue.Empty:
             if params['shutdown'].is_set():
                 return
-            else:
-                continue
+            continue
         wsdl_rel_path = ''
         try:
             wsdl_rel_path = xrdinfo.stringify(subsystem)
@@ -229,7 +237,7 @@ def worker(params):
                 if skip_methods:
                     # Skipping, because previous getWsdl request timed
                     # out
-                    logger.info('%s - SKIPPING', xrdinfo.stringify(method))
+                    LOGGER.info('%s - SKIPPING', xrdinfo.stringify(method))
                     method_index[xrdinfo.stringify(method)] = 'SKIPPED'
                     continue
 
@@ -240,16 +248,16 @@ def worker(params):
                 except xrdinfo.RequestTimeoutError:
                     # Skipping all following requests to that subsystem
                     skip_methods = True
-                    logger.info('%s - TIMEOUT', xrdinfo.stringify(method))
+                    LOGGER.info('%s - TIMEOUT', xrdinfo.stringify(method))
                     method_index[xrdinfo.stringify(method)] = 'TIMEOUT'
                     continue
-                except xrdinfo.XrdInfoError as e:
-                    if str(e) == 'SoapFault: Service is a REST service and does not have a WSDL':
+                except xrdinfo.XrdInfoError as err:
+                    if str(err) == 'SoapFault: Service is a REST service and does not have a WSDL':
                         # We do not want to spam messages about REST services
-                        logger.debug('%s: %s', xrdinfo.stringify(method), e)
+                        LOGGER.debug('%s: %s', xrdinfo.stringify(method), err)
                         method_index[xrdinfo.stringify(method)] = 'REST'
                     else:
-                        logger.info('%s: %s', xrdinfo.stringify(method), e)
+                        LOGGER.info('%s: %s', xrdinfo.stringify(method), err)
                         method_index[xrdinfo.stringify(method)] = ''
                     continue
 
@@ -260,99 +268,43 @@ def worker(params):
                         method_full_name = xrdinfo.stringify(subsystem + wsdl_method)
                         method_index[method_full_name] = '{}/{}'.format(wsdl_rel_path, wsdl_name)
                         txt = txt + '\n    {}'.format(method_full_name)
-                except xrdinfo.XrdInfoError as e:
-                    txt = txt + '\nWSDL parsing failed: {}'.format(e)
+                except xrdinfo.XrdInfoError as err:
+                    txt = txt + '\nWSDL parsing failed: {}'.format(err)
                     method_index[xrdinfo.stringify(method)] = ''
-                logger.info(txt)
+                LOGGER.info(txt)
 
                 if xrdinfo.stringify(method) not in method_index:
-                    logger.warning('{} - Method was not found in returned WSDL!'.format(
-                        xrdinfo.stringify(method)))
+                    LOGGER.warning(
+                        '%s - Method was not found in returned WSDL!', xrdinfo.stringify(method))
                     method_index[xrdinfo.stringify(method)] = ''
 
             with params['results_lock']:
                 params['results'][wsdl_rel_path] = {
                     'methods': method_index,
                     'ok': True}
-        except xrdinfo.XrdInfoError as e:
+        except xrdinfo.XrdInfoError as err:
             with params['results_lock']:
                 params['results'][wsdl_rel_path] = {
                     'methods': {},
                     'ok': False}
-            logger.info('%s: %s', xrdinfo.stringify(subsystem), e)
-        except Exception as e:
+            LOGGER.info('%s: %s', xrdinfo.stringify(subsystem), err)
+        except Exception as err:
             with params['results_lock']:
                 params['results'][wsdl_rel_path] = {
                     'methods': {},
                     'ok': False}
-            logger.warning('%s: %s', type(e).__name__, e)
+            LOGGER.warning('%s: %s', type(err).__name__, err)
         finally:
             params['work_queue'].task_done()
 
 
 def sort_by_time(item):
+    """A helper function for sorting, indicates which field to use"""
     return item['reportTime']
 
 
-def main():
-    logging.config.dictConfig(DEFAULT_LOGGER)
-
-    parser = argparse.ArgumentParser(
-        description='Collect WSDL service descriptions from X-Road members.',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog='By default peer TLS certificate is not validated.'
-    )
-    parser.add_argument(
-        'config', metavar='CONFIG_FILE',
-        help='Configuration file')
-    args = parser.parse_args()
-
-    config = load_config(args.config)
-    if config is None:
-        exit(1)
-
-    configure_logging(config)
-
-    params = set_params(config)
-    if params is None:
-        exit(1)
-
-    if not make_dirs(params['path']):
-        exit(1)
-
-    shared_params = None
-    try:
-        shared_params = xrdinfo.shared_params_ss(
-            addr=params['url'], instance=params['instance'], timeout=params['timeout'],
-            verify=params['verify'], cert=params['cert'])
-    except xrdinfo.XrdInfoError as e:
-        logger.error('Cannot download Global Configuration: %s', e)
-        exit(1)
-
-    # Create and start new threads
-    threads = []
-    for _ in range(params['thread_cnt']):
-        t = Thread(target=worker, args=(params,))
-        t.daemon = True
-        t.start()
-        threads.append(t)
-
-    # Populate the queue
-    try:
-        for subsystem in xrdinfo.registered_subsystems(shared_params):
-            params['work_queue'].put(subsystem)
-    except xrdinfo.XrdInfoError as e:
-        logger.error('Cannot process Global Configuration: %s', e)
-        exit(1)
-
-    # Block until all tasks in queue are done
-    params['work_queue'].join()
-
-    # Set shutdown event and wait until all daemon processes finish
-    params['shutdown'].set()
-    for t in threads:
-        t.join()
-
+def process_results(params):
+    """Process results collected by worker threads"""
     results = params['results']
 
     card_nr = 0
@@ -361,7 +313,7 @@ def main():
         card_nr += 1
         subsystem_result = results[subsystem_key]  # type: dict
         methods = subsystem_result['methods']
-        if subsystem_result['ok'] and len(methods) > 0:
+        if subsystem_result['ok'] and methods:
             subsystem_status = 'ok'
         elif subsystem_result['ok']:
             subsystem_status = 'empty'
@@ -406,26 +358,89 @@ def main():
     suffix = time.strftime('%Y%m%d%H%M%S', report_time)
 
     # JSON output
-    with open('{}/index_{}.json'.format(params['path'], suffix), 'w') as f:
-        json.dump(json_data, f, indent=2, ensure_ascii=False)
+    with open('{}/index_{}.json'.format(params['path'], suffix), 'w') as json_file:
+        json.dump(json_data, json_file, indent=2, ensure_ascii=False)
 
     json_history = []
     try:
-        with open('{}/history.json'.format(params['path']), 'r') as f:
-            json_history = json.load(f)
+        with open('{}/history.json'.format(params['path']), 'r') as json_file:
+            json_history = json.load(json_file)
     except IOError:
-        logger.info('History file history.json not found')
+        LOGGER.info('History file history.json not found')
 
     json_history.append({'reportTime': formatted_time, 'reportPath': 'index_{}.json'.format(
         suffix)})
     json_history.sort(key=sort_by_time, reverse=True)
 
-    with open('{}/history.json'.format(params['path']), 'w') as f:
-        json.dump(json_history, f, indent=2, ensure_ascii=False)
+    with open('{}/history.json'.format(params['path']), 'w') as json_file:
+        json.dump(json_history, json_file, indent=2, ensure_ascii=False)
 
     # Replace index.json with latest report
     shutil.copy(
         '{}/index_{}.json'.format(params['path'], suffix), '{}/index.json'.format(params['path']))
+
+
+def main():
+    """Main function"""
+    logging.config.dictConfig(DEFAULT_LOGGER)
+
+    parser = argparse.ArgumentParser(
+        description='Collect WSDL service descriptions from X-Road members.',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog='By default peer TLS certificate is not validated.'
+    )
+    parser.add_argument(
+        'config', metavar='CONFIG_FILE',
+        help='Configuration file')
+    args = parser.parse_args()
+
+    config = load_config(args.config)
+    if config is None:
+        exit(1)
+
+    configure_logging(config)
+
+    params = set_params(config)
+    if params is None:
+        exit(1)
+
+    if not make_dirs(params['path']):
+        exit(1)
+
+    shared_params = None
+    try:
+        shared_params = xrdinfo.shared_params_ss(
+            addr=params['url'], instance=params['instance'], timeout=params['timeout'],
+            verify=params['verify'], cert=params['cert'])
+    except xrdinfo.XrdInfoError as err:
+        LOGGER.error('Cannot download Global Configuration: %s', err)
+        exit(1)
+
+    # Create and start new threads
+    threads = []
+    for _ in range(params['thread_cnt']):
+        thread = Thread(target=worker, args=(params,))
+        thread.daemon = True
+        thread.start()
+        threads.append(thread)
+
+    # Populate the queue
+    try:
+        for subsystem in xrdinfo.registered_subsystems(shared_params):
+            params['work_queue'].put(subsystem)
+    except xrdinfo.XrdInfoError as err:
+        LOGGER.error('Cannot process Global Configuration: %s', err)
+        exit(1)
+
+    # Block until all tasks in queue are done
+    params['work_queue'].join()
+
+    # Set shutdown event and wait until all daemon processes finish
+    params['shutdown'].set()
+    for thread in threads:
+        thread.join()
+
+    process_results(params)
 
 
 if __name__ == '__main__':
